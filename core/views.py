cdcf7153d7063b43e39d4165f16e6aa28501c1f9
Eduardo Francellino <edfrancell@gmail.com>
2010-10-31 04:45:18 -0200

    Commit inicial

---
+++ core/views.py
@@ -0,0 +1,6 @@
+# Create your views here.
+
+from django.http import HttpResponse
+
+def homepage(request):
+    return HttpResponse('Bem-vindo ao EventeX')
---
+++ urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls.defaults import *
+from core.views import *
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^eventex/', include('eventex.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # (r'^admin/', include(admin.site.urls)),
+    (r'^$', homepage),
+)
