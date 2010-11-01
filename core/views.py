95e385fce41634ca336f1d56153ba69bb1a715b0
Eduardo Francellino <edfrancell@gmail.com>
2010-11-01 02:25:15 -0200

    Configuracao das templates, Inclusao da template principal

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
-# Create your views here.
-
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def homepage(request):
-    return HttpResponse('Bem-vindo ao EventeX')
+    from django.conf import settings
+    context = {'MEDIA_URL': settings.MEDIA_URL}
+
+    return render_to_response('index.html', context)
--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from core.views import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -16,3 +17,10 @@ urlpatterns = patterns('',
     # (r'^admin/', include(admin.site.urls)),
     (r'^$', homepage),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$',
+         'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
