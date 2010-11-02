030a4f818a510a0f5c2779c607af686b2930c3f1
Eduardo Francellino <edfrancell@gmail.com>
2010-11-02 16:35:23 -0200

    Fix media admin and fixture for admin created

--- settings.py
+++ settings.py
@@ -58,7 +58,8 @@ MEDIA_URL = '/media/'
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = MEDIA_URL + 'admin/'
+# ADMIN_MEDIA_PREFIX = MEDIA_URL + 'admin/'
+ADMIN_MEDIA_PREFIX = '/media-admin/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'b+pa1bn0^pvj1qr!gmx!qs7=@za4^+6reo0ip3g33s++i5bq#e'
