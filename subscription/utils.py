fb8cd723eb0f73887ee119aace2d0fb1f209d943
Eduardo Francellino <edfrancell@gmail.com>
2010-11-03 00:08:24 -0200

    Fix encode of utils lib

--- subscription/utils.py
+++ subscription/utils.py
@@ -3,19 +3,20 @@ from django.template.loader import render_to_string
 from django.core.mail import send_mail
 from django.conf import settings
 
+
 def send_subscription_email(subscription):
-	if subscription is None:
-		raise ValueError('This function must be connected to: post_save of Subscription')
-	subject = 'Inscrição no EventeX realizada com sucesso.'
-	
-	message = render_to_string(
-		'subscription/email.txt',
-		{'subscription': subscription }
-	)
-	
-	send_mail(
-		subject = subject,
-		message = message,
-		from_email = settings.DEFAULT_FROM_EMAIL,
-		recipient_list = [ subscription.email ],
-	)
+    if subscription is None:
+        raise ValueError('This function must be connected to: post_save of Subscription')
+
+    subject = u'InscriÃ§Ã£o no EventeX realizada com sucesso.'
+    message = render_to_string(
+        'subscription/email.txt',
+        {'subscription': subscription }
+    )
+
+    send_mail(
+        subject = subject,
+        message = message,
+        from_email = settings.DEFAULT_FROM_EMAIL,
+        recipient_list = [ subscription.email ],
+    )
